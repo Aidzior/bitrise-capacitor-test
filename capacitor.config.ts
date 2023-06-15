dc90977c31a7888150886de89ff071a92eb08f03
andrzejidzior <andrzej.idzior@appyourself.net>
2023-06-15 11:13:09 +0200

    appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ay.ionic.capacitor.starter',
+  appId: 'com.klawiatura.app',
   appName: 'host',
   webDir: 'www',
   server: {
