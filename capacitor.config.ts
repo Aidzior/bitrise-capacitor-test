020db86573dab0cdead0460533defc709e062e0e
andrzejidzior <andrzej.idzior@appyourself.net>
2023-06-15 09:56:02 +0200

    appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'ay.ionic.capacitor-starter',
+  appId: 'ay.ionic.capacitor.starter',
   appName: 'host',
   webDir: 'www',
   server: {
