852e072f998072e7c0d9449e27837e4bd5e08fde
andrzejidzior <andrzej.idzior@appyourself.net>
2023-06-15 11:38:26 +0200

    appId

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.klawiatura.app',
-  appName: 'host',
+  appName: 'KLAWIATURA',
   webDir: 'www',
   server: {
     androidScheme: 'https'
